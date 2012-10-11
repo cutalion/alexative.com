67f1e83efa4da94e2348085dca00cd1ab35a7e75
Alexander Glushkov <cutalion@gmail.com>
2012-10-11 18:14:18 +0300

    IE version of site

--- alexative_app.rb
+++ alexative_app.rb
@@ -3,7 +3,12 @@ class AlexativeApp < Sinatra::Base
   set :haml, format: :html5
 
   get "/" do
-    haml :index
+    user_agent = UserAgent.new env["HTTP_USER_AGENT"].to_s
+    if user_agent.name == :ie
+      erb :ie, content_type: "text/plain"
+    else
+      haml :index
+    end
   end
 
   get "/application.css" do
